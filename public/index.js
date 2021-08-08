8c723c53469cd8c163787de529741c88d75f9045
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2021-08-07 20:27:27 -0500

    first working counter

---
+++ public/index.js
@@ -0,0 +1,27 @@
+
+
+// COUNT FUNCS
+let counter = document.getElementById("count")
+let count = 0
+function updateDisplay(){
+    counter.innerHTML = count;
+};
+updateDisplay();
+
+// BUTTON CODE
+let increment = document.getElementById("inc")
+let decrement = document.getElementById("dec")
+
+increment.addEventListener("click", ()=>{
+    count++;
+    updateDisplay();
+});
+
+decrement.addEventListener("click", ()=>{
+    count--;
+    updateDisplay();
+});
+
+function pushChange(){
+
+}
